25ed1ab424a15ae278fc52ad0e2783ca5f66e5c3
Adishenz <41304894+Adishenz@users.noreply.github.com>
2021-07-26 13:17:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title('Hello')
+    st.subheader('This is a test run')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[theme]\n\
+primaryColor = ‘#84a3a7’\n\
+backgroundColor = ‘#EFEDE8’\n\
+secondaryBackgroundColor = ‘#fafafa’\n\
+textColor= ‘#424242’\n\
+font = ‘sans serif’\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+" > ~/.streamlit/config.toml
